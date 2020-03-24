7d28e925c92da5f3c81d715265d01a9d4fe9d411
Sebastien Duc <sduc@palantir.com>
2020-03-23 20:00:56 -0400

    fix  script

--- script/copy.sh
+++ script/copy.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-current_date=$(date +'%y-%m-%d')
+current_date=$(date +'%Y-%m-%d')
 curl "https://www.ecdc.europa.eu/sites/default/files/documents/COVID-19-geographic-disbtribution-worldwide-${current_date}.xlsx" --output COVID-19-geographic-distribution-worldwide.xlsx
 
 git clone git@github.com:sduc/ecdc-covid-19.git
