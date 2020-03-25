33deb2cb6f8b4e48c5c02bc7151f866a44fc6ad6
warmenos <warmenos@gmail.com>
2020-03-25 18:22:47 +0000

    Update copy.sh

--- script/copy.sh
+++ script/copy.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 current_date=$(date +'%Y-%m-%d')
-curl "https://www.ecdc.europa.eu/sites/default/files/documents/COVID-19-geographic-disbtribution-worldwide-${current_date}.xlsx" --output COVID-19-geographic-distribution-worldwide.xlsx
+curl "https://www.ecdc.europa.eu/sites/default/files/documents/COVID-19-geographic-disbtribution-worldwide-${current_date}.csv" --output COVID-19-geographic-distribution-worldwide.csv
 
 git clone git@github.com:sduc/ecdc-covid-19.git
-mv COVID-19-geographic-distribution-worldwide.xlsx ecdc-covid-19/current/
+mv COVID-19-geographic-distribution-worldwide.csv ecdc-covid-19/current/
 cd ecdc-covid-19
 git add .
 git commit -m "Updating file"
